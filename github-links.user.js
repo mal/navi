1abe94875542b394cd011ffda51fdfd8d2623727
Mal Graty <mal.graty@googlemail.com>
2013-01-12 05:00:58 +0000

    Switch to using MutationObserver
    Also use forEach and some on Arrays and NodeLists.

--- github-links.user.js
+++ github-links.user.js
@@ -1,94 +1,93 @@
 !function () {
 
-    var base, index, tree;
+    // vars and shit
+    var base, index, tree,
+        MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
     // first things first
     function init() {
+        // easy mode
+        HTMLCollection.prototype.forEach = NodeList.prototype.forEach = Array.prototype.forEach;
+        HTMLCollection.prototype.some = NodeList.prototype.some = Array.prototype.some;
+        MutationObserver.prototype.watch = watch;
+
         // populate base and tree variables
-        foreach.call(
-            document.getElementsByTagName('link'),
-            function (link) {
-                if ( link.rel === 'alternate' && link.type === 'application/atom+xml' )
-                    base = rewrite(link.href.substr(0, link.href.indexOf('.atom')));
-                if ( link.rel === 'permalink' )
-                    tree = link.href.split('/')[6];
-                if ( base && tree )
-                    return;
-            }
-        );
+        document.getElementsByTagName('link').some(function (link) {
+            if ( link.rel === 'alternate' && link.type === 'application/atom+xml' )
+                base = rewrite(link.href.substr(0, link.href.indexOf('.atom')));
+            if ( link.rel === 'permalink' )
+                tree = link.href.split('/')[6];
+            return base && tree;
+        });
 
         // unsupported page
-        if ( ! base ) return;
+        if ( ! base )
+            return;
 
-        // couldn't find tree, try elsewhere (mainly for edit previews)
-        if ( ! tree ) {
-            tree = foreach.call(
-                document.getElementsByName('commit'),
-                function (commit) {
-                    return commit.value;
-                }
-            );
-        }
+        // couldn't find tree, try elsewhere (for edit pages)
+        if ( ! tree )
+            document.getElementsByName('commit').some(function (commit) {
+                return tree = commit.value;
+            });
 
-        // lookup file list and generate index; disallow duplicate names
-        list(function (paths) {
+        // lookup file list and generate index
+        files(function (paths) {
             index = {};
-            foreach.call(paths, function (path) {
+
+            // store file:path pairs, null repeat filenames
+            paths.forEach(function (path) {
                 var name = path.substr(path.lastIndexOf('/') + 1);
-                if ( ! ( name in index ) )
+                if ( ! (name in index) )
                     index[name] = path;
                 else
                     index[name] = null;
             });
+
+            // clear nulls leaving unique filenames only
             for ( name in index )
                 if ( index[name] === null )
                     delete index[name];
-            // re-mutate once populate
+
+            // re-mutate once populated
             mutate();
         });
 
-        // mutate when entering a page view using ajax slider
-        watch(document.getElementById('slider'), function (e) {
-            if ( e.target.className === 'frame' )
-                mutate();
+        // configure observer to watch for DOM changes
+        var mo = new MutationObserver(function (mutations) {
+            mutations.some(function (mutation) {
+                return mutation.addedNodes.some(function (node) {
+                    if ( node.id === 'readme' || node.querySelector('#readme') )
+                        return mutate(), true;
+                });
+            });
         });
 
+        // mutate when entering a page view using ajax slider
+        mo.watch('#slider .frames');
+
         // mutate when entering an edit page's preview mode
-        watch(document.getElementsByClassName('js-commit-preview')[0], function (e) {
-            if ( e.target.id === 'readme' )
-                mutate();
-        });
+        mo.watch('#files .js-commit-preview');
 
         // mutate page as is to fix everything
         mutate();
     }
 
-    // neater loops; with return values
-    function foreach(_) {
-        var i = 0,
-            l = this.length,
-            r;
-
-        for ( ; i < l; i++ )
-            if ( r = _(this[i], i) )
-                return r;
-    }
-
     // fetch file list from github
-    function list(_) {
+    function files(callback) {
         // no tree? bail
-        if ( ! tree ) return;
+        if ( ! tree )
+            return;
 
         // can haz cache?
         if ( typeof tree === 'object' )
-            return _(tree);
+            return callback(tree);
 
         var xhr = new XMLHttpRequest();
 
         xhr.onreadystatechange = function () {
             if ( xhr.readyState === 4 && xhr.status === 200 ) {
                 tree = JSON.parse(xhr.responseText).paths;
-                _(tree);
+                callback(tree);
             }
         }
 
@@ -101,29 +100,25 @@
         var readme = document.getElementById('readme');
 
         // nowhere to fix things
-        if ( ! readme ) return;
+        if ( ! readme )
+            return;
 
-        // fix anchors
-        foreach.call(
-            readme.getElementsByTagName('a'),
-            function (a) {
-                if ( a.className === 'anchor' )
-                    return;
-                var url = rewrite(a.href);
-                if ( url )
-                    a.href = url;
-            }
-        );
+        // fix links
+        readme.getElementsByTagName('a').forEach(function (a) {
+            // skip anchors
+            if ( a.className === 'anchor' )
+                return;
+            var url = rewrite(a.href);
+            if ( url )
+                a.href = url;
+        });
 
         // fix images
-        foreach.call(
-            readme.getElementsByTagName('img'),
-            function (img) {
-                var url = rewrite(img.src, true);
-                if ( url )
-                    img.src = url;
-            }
-        );
+        readme.getElementsByTagName('img').forEach(function (img) {
+            var url = rewrite(img.src, true);
+            if ( url )
+                img.src = url;
+        });
     }
 
     // magical rewriting
@@ -132,6 +127,7 @@
         var url = search(uri),
             parts = /([a-z0-9]+:\/\/(?:[^\/]+\/){3})([^\/]+)(\/.*?)(?:\.\.\.(\/.*))?$/.exec(url);
 
+        // return false on no change
         function out(url) {
             if ( uri === url )
                 return false;
@@ -168,29 +164,35 @@
 
     // lookup in index
     function search(uri) {
-        if ( base && index ) {
-            // extract stem, strip stem from uri, validate uri is now pathless
-            var location = window.location.toString(),
-                pathless = /^([^\?\/#]+)?(\?[^#]+)?(#.+)?$/.exec(
-                    uri.replace(location.substr(0, location.lastIndexOf('/') + 1), '')
-                );
+        if ( ! (base && index) )
+            return uri;
 
-            // lookup pathless urls
-            if ( pathless ) {
-                pathless.shift();
-                var name = pathless.shift();
-                if ( name in index )
-                    uri = base.join('') + '/' + index[name] + pathless.join('');
-            }
+        // extract stem, strip stem from uri, validate uri is now pathless
+        var location = window.location.toString(),
+            pathless = /^([^\?\/#]+)?(\?[^#]+)?(#.+)?$/.exec(
+                uri.replace(location.substr(0, location.lastIndexOf('/') + 1), '')
+            );
+
+        // lookup pathless urls
+        if ( pathless ) {
+            pathless.shift();
+            var name = pathless.shift();
+            if ( name in index )
+                uri = base.join('') + '/' + index[name] + pathless.join('');
         }
 
         return uri;
     }
 
-    // attach DOMNodeInserted listener
-    function watch(el, handler) {
+    // start observing childLists on element
+    function watch(el) {
+        // assume selector if string
+        if ( typeof el === 'string' )
+            el = document.querySelector(el);
+
+        // ignore if not valid element
         if ( el instanceof HTMLElement )
-            el.addEventListener('DOMNodeInserted', handler);
+            this.observe(el, { attributes: false, childList: true, characterData: false });
     }
 
     init();
