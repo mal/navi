9f901aeef8196508f1a9a2a35d46a0dac4e91eff
Mal Graty <mal.graty@googlemail.com>
2013-01-18 02:00:25 +0000

    Better 16px icon

--- src/github-links.user.js
+++ src/github-links.user.js
@@ -3,7 +3,7 @@
 // @description Fix relative links in readmes and other text files on GitHub
 // @namespace   com.github.mal
 // @match       https://github.com/*/*
-// @version     0.4.3
+// @version     0.4.4
 // @grant       none
 // @icon        https://raw.github.com/mal/github-links/master/src/icons/48.png
 // @downloadURL https://raw.github.com/mal/github-links/master/src/github-links.user.js
